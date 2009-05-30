sysfce2/Shareaza

commit bfc421b1bc6c667abd3380e0c9b66fc583248015

Fixed CMatchList counters (m_nFilteredHits, m_nGnutellaHits, m_nED2KHits). Fixed search window slow refresh after sanity check. Fixed search window concurrent sanity checks. Changed ban default timeout from Session to Infinite. Added banning by all hashes (added CQueryHit::Ban and CMatchFile:Ban methods). Enabled search window file (including hits) ban. Refactored/optimized CSecurityRule::Match and CSecurity::IsDenied methods.
git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@7112 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/MatchObjects.cpp
@@ -176,9 +176,8 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 		// The size may be zero or match the size of the file.
 		// Empty file names are caught by the next clause and deleted.
 
-		if ( Security.IsDenied( &pHit->m_pAddress, pHit->m_sName ) || pHit->m_sName.IsEmpty() ||
-			 Security.IsDenied( pHit->m_sName, pHit->m_nSize, pHit->m_oSHA1, pHit->m_oED2K ) || 
-			 pHit->m_nSize == 0 )
+		if ( pHit->m_nSize == 0 || pHit->m_sName.IsEmpty() ||
+			Security.IsDenied( &pHit->m_pAddress ) || Security.IsDenied( pHit ) )
 		{
 			delete pHit;
 			pHit = pNext;
@@ -239,7 +238,6 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 		FilterHit( pHit );
 		
 		CMatchFile* pFile	= NULL;
-		PROTOCOLID nProtocol= pHit->m_nProtocol;
 		FILESTATS Stats = {};
 
 		if ( pHit->m_oSHA1 )
@@ -270,10 +268,11 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 			pFile = FindFileAndAddHit( pHit, fSize, &Stats );
 		}
 		
-		if ( pFile != NULL ) // New hit for the existing file
+		bool bExistingFile = ( pFile != NULL );
+		if ( bExistingFile )
 		{
+			// New hit for the existing file
 			pMap = m_pFiles;
-
 			for ( DWORD nCount = m_nFiles ; nCount ; nCount--, pMap++ )
 			{
 				if ( *pMap == pFile )
@@ -289,37 +288,13 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 					{
 						UpdateRange( m_nFiles - nCount, m_nFiles - nCount );
 					}
-
 					break;
 				}
-			}
-			
-			if ( Stats.nHadCount )
-			{
-				ASSERT( m_nItems >= (UINT)Stats.nHadCount );
-				m_nItems -= Stats.nHadCount;
-				ASSERT( m_nFilteredFiles );
-				m_nFilteredFiles --;
-				ASSERT( m_nFilteredHits >= (UINT)Stats.nHadFilteredGnutella + Stats.nHadFilteredED2K );
-				m_nFilteredHits -= ( Stats.nHadFilteredGnutella + Stats.nHadFilteredED2K );
-				switch ( nProtocol )
-				{
-				case PROTOCOL_G1:
-				case PROTOCOL_G2:
-					if ( m_nGnutellaHits >= (UINT)Stats.nHadFilteredGnutella )
-						m_nGnutellaHits -= Stats.nHadFilteredGnutella;
-					break;
-				case PROTOCOL_ED2K:
-					if ( m_nED2KHits >= (UINT)Stats.nHadFilteredED2K )
-						m_nED2KHits -= Stats.nHadFilteredED2K;
-					break;
-				default:
-					;
-				}
-			}
+			}			
 		}
-		else // New file hit
+		else
 		{
+			// New file hit
 			pFile = new CMatchFile( this, pHit );
 			pFile->m_bNew = m_bNew;
 			
@@ -349,6 +324,23 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 			{
 				UpdateRange( m_nFiles );
 				m_pFiles[ m_nFiles++ ] = pFile;
+			}
+
+			if ( DWORD nItemCount = pFile->GetItemCount() )
+			{
+				m_nItems += nItemCount;
+				m_nFilteredFiles ++;
+				m_nFilteredHits += pFile->m_nFiltered;
+				switch ( pHit->m_nProtocol )
+				{
+				case PROTOCOL_G1:
+				case PROTOCOL_G2:
+					m_nGnutellaHits += pFile->m_nFiltered;
+					break;
+				case PROTOCOL_ED2K:
+					m_nED2KHits += pFile->m_nFiltered;
+					break;
+				}
 			}
 		}
 		
@@ -381,35 +373,6 @@ void CMatchList::AddHits(CQueryHit* pHit, CQuerySearch* pFilter)
 			pMap = m_pMapMD5 + pFile->m_oMD5[ 0 ];
 			pFile->m_pNextMD5 = *pMap;
 			*pMap = pFile;
-		}
-		
-		Stats.nHadCount = pFile->GetItemCount();
-		
-		if ( Stats.nHadCount )
-		{
-			m_nItems += Stats.nHadCount;
-			m_nFilteredFiles ++;
-			m_nFilteredHits += pFile->m_nFiltered;
-
-			switch ( nProtocol )
-			{
-			case PROTOCOL_G1:
-			case PROTOCOL_G2:
-				m_nGnutellaHits += pFile->m_nFiltered;
-				break;
-			case PROTOCOL_ED2K:
-				m_nED2KHits += pFile->m_nFiltered;
-				break;
-			case PROTOCOL_BT:
-			case PROTOCOL_FTP:
-			case PROTOCOL_HTTP:
-			case PROTOCOL_NULL:
-			case PROTOCOL_ANY:
-			default:
-//				ASSERT( 0 )
-				;
-			}
-
 		}
 		
 		pHit = pNext;
@@ -470,36 +433,18 @@ CMatchFile* CMatchList::FindFileAndAddHit(CQueryHit* pHit, const findType nFindF
 
 		if ( bValid )
 		{
-			Stats->bHad[0] = bool( pSeek->m_oSHA1 );
-			Stats->bHad[1] = bool( pSeek->m_oTiger );
-			Stats->bHad[2] = bool( pSeek->m_oED2K );
-			Stats->bHad[3] = bool( pSeek->m_oBTH );
-			Stats->bHad[4] = bool( pSeek->m_oMD5 );
-
+			bool bHadSHA1 = pSeek->m_oSHA1;
+			bool bHadTiger = pSeek->m_oTiger;
+			bool bHadED2K = pSeek->m_oED2K;
+			bool bHadBTH = pSeek->m_oBTH;
+			bool bHadMD5 = pSeek->m_oMD5;
 			if ( pSeek->Add( pHit, ( nFindFlag != fSize ) ) )
 			{
-				Stats->bHadSHA1	 |= Stats->bHad[0];
-				Stats->bHadTiger |= Stats->bHad[1];
-				Stats->bHadED2K	 |= Stats->bHad[2];
-				Stats->bHadBTH	 |= Stats->bHad[3];
-				Stats->bHadMD5	 |= Stats->bHad[4];
-
-				ASSERT( Stats->nHadCount == 0 );
-				Stats->nHadCount = pSeek->GetItemCount();
-				switch ( pHit->m_nProtocol )
-				{
-				case PROTOCOL_G1:
-				case PROTOCOL_G2:
-					ASSERT( Stats->nHadFilteredGnutella == 0 );
-					Stats->nHadFilteredGnutella	= pSeek->m_nFiltered;
-					break;
-				case PROTOCOL_ED2K:
-					ASSERT( Stats->nHadFilteredED2K	== 0 );
-					Stats->nHadFilteredED2K	= pSeek->m_nFiltered;
-					break;
-				default:
-					;
-				}
+				Stats->bHadSHA1	 |= bHadSHA1;
+				Stats->bHadTiger |= bHadTiger;
+				Stats->bHadED2K	 |= bHadED2K;
+				Stats->bHadBTH	 |= bHadBTH;
+				Stats->bHadMD5	 |= bHadMD5;
 				return pSeek;
 			}
 			//else
@@ -508,11 +453,11 @@ CMatchFile* CMatchList::FindFileAndAddHit(CQueryHit* pHit, const findType nFindF
 
 		if ( nFindFlag == fSize )
 		{
-			Stats->bHadSHA1 = FALSE;
-			Stats->bHadTiger = FALSE;
-			Stats->bHadED2K = FALSE;
-			Stats->bHadBTH = FALSE;
-			Stats->bHadMD5 = FALSE;
+			Stats->bHadSHA1 = false;
+			Stats->bHadTiger = false;
+			Stats->bHadED2K = false;
+			Stats->bHadBTH = false;
+			Stats->bHadMD5 = false;
 		}
 
 		switch( nFindFlag )
@@ -967,16 +912,15 @@ BOOL CMatchList::FilterHit(CQueryHit* pHit)
 		}
 	}
 
-	if ( m_bFilterBusy && pHit->m_bBusy == TRI_TRUE ) return FALSE;
-	//if ( m_bFilterPush && pHit->m_bPush == TRI_TRUE && pHit->m_nProtocol != PROTOCOL_ED2K ) return FALSE;
-	if ( m_bFilterPush && pHit->m_bPush == TRI_TRUE ) return FALSE;
-	if ( m_bFilterUnstable && pHit->m_bStable == TRI_FALSE ) return FALSE;
-	if ( m_bFilterReject && pHit->m_bMatched == FALSE ) return FALSE;
-	if ( m_bFilterBogus && pHit->m_bBogus ) 
+	if ( ( m_bFilterBusy && pHit->m_bBusy == TRI_TRUE ) ||
+	//	( m_bFilterPush && pHit->m_bPush == TRI_TRUE && pHit->m_nProtocol != PROTOCOL_ED2K ) ||
+		( m_bFilterPush && pHit->m_bPush == TRI_TRUE ) ||
+		( m_bFilterUnstable && pHit->m_bStable == TRI_FALSE ) ||
+		( m_bFilterReject && pHit->m_bMatched == FALSE ) ||
+		( m_bFilterBogus && pHit->m_bBogus ) ||
+		( m_nFilterMinSize > 0 && pHit->m_nSize < m_nFilterMinSize ) ||
+		( m_nFilterMaxSize > 0 && pHit->m_nSize > m_nFilterMaxSize ) )
 		return FALSE;
-	
-	if ( m_nFilterMinSize > 0 && pHit->m_nSize < m_nFilterMinSize ) return FALSE;
-	if ( m_nFilterMaxSize > 0 && pHit->m_nSize > m_nFilterMaxSize ) return FALSE;
 	
 	if ( m_pszFilter )
 	{
@@ -2681,4 +2625,14 @@ void CMatchFile::SanityCheck()
 
 		pHit = pNext;
 	}
+}
+
+void CMatchFile::Ban(int nBanLength)
+{
+	// Ban by hit host IPs
+	for ( CQueryHit* pHit = m_pHits ; pHit ; pHit = pHit->m_pNext )
+		pHit->Ban( nBanLength );
+
+	// Ban hashes
+	Security.Ban( this, nBanLength );
 }

shareaza/MatchObjects.h
@@ -40,15 +40,11 @@ class CBaseMatchWnd;
 
 typedef struct
 {
-	BOOL	bHadSHA1;
-	BOOL	bHadTiger;
-	BOOL	bHadED2K;
-	BOOL	bHadBTH;
-	BOOL	bHadMD5;
-	DWORD	nHadCount;
-	DWORD	nHadFilteredGnutella;
-	DWORD	nHadFilteredED2K;
-	BOOL	bHad[5];
+	bool	bHadSHA1;
+	bool	bHadTiger;
+	bool	bHadED2K;
+	bool	bHadBTH;
+	bool	bHadMD5;
 } FILESTATS;
 
 class CMatchList
@@ -196,26 +192,26 @@ public:
 	inline int	Compare(CMatchFile* pFile) const;
 	CString		GetURN() const;
 	void		Serialize(CArchive& ar, int nVersion);
-
+	void		Ban(int nBanLength);	// Ban by hashes and by hit host IPs
 	
 	inline DWORD GetFilteredCount()
 	{
-		if ( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE ) return 0;
-		if ( m_pList->m_bFilterDRM && m_bDRM ) return 0;
-		if ( m_pList->m_bFilterSuspicious && m_bSuspicious ) return 0;
-		if ( m_nSources < m_pList->m_nFilterSources ) return 0;
-		if ( m_pBest == NULL ) return 0;
+		if ( ( m_pList->m_bFilterDRM && m_bDRM ) ||
+			( m_pList->m_bFilterSuspicious && m_bSuspicious ) ||
+			( m_nSources < m_pList->m_nFilterSources ) ||
+			( m_pBest == NULL ) ||
+			( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE ) ) return 0;
 
 		return m_nFiltered;
 	}
 	
 	inline DWORD GetItemCount()
 	{
-		if ( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE )return 0;
-		if ( m_pList->m_bFilterDRM && m_bDRM ) return 0;
-		if ( m_pList->m_bFilterSuspicious && m_bSuspicious ) return 0;
-		if ( m_nSources < m_pList->m_nFilterSources ) return 0;
-		if ( m_pBest == NULL ) return 0;
+		if ( ( m_pList->m_bFilterDRM && m_bDRM ) ||
+			( m_pList->m_bFilterSuspicious && m_bSuspicious ) ||
+			( m_nSources < m_pList->m_nFilterSources ) ||
+			( m_pBest == NULL ) ||
+			( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE ) ) return 0;
 
 		if ( m_nFiltered == 1 || ! m_bExpanded )
 			return 1;

shareaza/QueryHit.cpp
@@ -440,7 +440,7 @@ CQueryHit* CQueryHit::FromPacket(CG2Packet* pPacket, int* pnHops)
 				{
 					nAddress = pPacket->ReadLongLE();
 					if ( Network.IsReserved( (IN_ADDR*)&nAddress, false ) ||
-						 Security.IsDenied( (IN_ADDR*)&nAddress, NULL ) )
+						 Security.IsDenied( (IN_ADDR*)&nAddress ) )
 						bSpam = true;
 					nPort = pPacket->ReadShortBE();
 				}
@@ -1981,4 +1981,9 @@ void CQueryHit::Serialize(CArchive& ar, int nVersion)
 		
 		if ( m_nSources == 0 && m_sURL.GetLength() ) m_nSources = 1;
 	}
+}
+
+void CQueryHit::Ban(int nBanLength)
+{
+	Security.Ban( &m_pAddress, nBanLength );
 }

shareaza/QueryHit.h
@@ -103,6 +103,7 @@ public:
 	void		Delete();
 	int			GetRating();
 	void		Serialize(CArchive& ar, int nVersion);
+	void		Ban(int nBanLength);	// Ban by host IP only
 protected:
 	void		ReadG1Packet(CG1Packet* pPacket);
 	void		ParseAttributes(const Hashes::Guid& pClientID, CVendor* pVendor, BYTE* nFlags, BOOL bChat, BOOL bBrowseHost);

shareaza/Security.cpp
@@ -201,18 +201,18 @@ void CSecurity::Clear()
 //////////////////////////////////////////////////////////////////////
 // CSecurity ban
 
-void CSecurity::Ban(const IN_ADDR* pAddress, int nBanLength, BOOL bMessage)
+void CSecurity::BanHelper(const IN_ADDR* pAddress, const CShareazaFile* pFile, int nBanLength, BOOL bMessage)
 {
 	CQuickLock oLock( m_pSection );
 
 	DWORD tNow = static_cast< DWORD >( time( NULL ) );
-	CString strAddress = inet_ntoa( *pAddress );
 
 	for ( POSITION pos = GetIterator() ; pos ; )
 	{
 		CSecureRule* pRule = GetNext( pos );
 
-		if ( pRule->Match( pAddress ) )
+		if ( ( pRule->m_nType == CSecureRule::srAddress && pRule->Match( pAddress ) ) ||
+			 ( pRule->m_nType == CSecureRule::srContent && pRule->Match( pFile ) ) )
 		{
 			if ( pRule->m_nAction == CSecureRule::srDeny )
 			{
@@ -224,19 +224,18 @@ void CSecurity::Ban(const IN_ADDR* pAddress, int nBanLength, BOOL bMessage)
 				{
 					pRule->m_nExpire = CSecureRule::srIndefinite;
 				}
-				else if ( bMessage )
+				else if ( bMessage && pAddress )
 				{
 					theApp.Message( MSG_NOTICE, IDS_NETWORK_SECURITY_ALREADY_BLOCKED,
-						(LPCTSTR)strAddress );
+						(LPCTSTR)CString( inet_ntoa( *pAddress ) ) );
 				}
-
 				return;
 			}
 		}
 	}
 
-	CSecureRule* pRule	= new CSecureRule();
-	pRule->m_nAction	= CSecureRule::srDeny;
+	CSecureRule* pRule = new CSecureRule();
+	pRule->m_nAction = CSecureRule::srDeny;
 
 	switch ( nBanLength )
 	{
@@ -269,13 +268,26 @@ void CSecurity::Ban(const IN_ADDR* pAddress, int nBanLength, BOOL bMessage)
 		pRule->m_sComment	= _T("Quick Ban");
 	}
 
-	CopyMemory( pRule->m_nIP, pAddress, sizeof pRule->m_nIP );
+	if ( pAddress )
+		CopyMemory( pRule->m_nIP, pAddress, sizeof pRule->m_nIP );
+	else if ( pFile && ( pFile->m_oSHA1 || pFile->m_oED2K || pFile->m_oTiger ||
+		pFile->m_oMD5 || pFile->m_oBTH ) )
+	{
+		pRule->m_nType = CSecureRule::srContent;
+		pRule->SetContentWords(
+			( pFile->m_oSHA1  ? pFile->m_oSHA1.toUrn()  + _T(" ") : CString() ) +
+			( pFile->m_oED2K  ? pFile->m_oED2K.toUrn()  + _T(" ") : CString() ) +
+			( pFile->m_oTiger ? pFile->m_oTiger.toUrn() + _T(" ") : CString() ) +
+			( pFile->m_oMD5   ? pFile->m_oMD5.toUrn()   + _T(" ") : CString() ) +
+			( pFile->m_oBTH   ? pFile->m_oBTH.toUrn()             : CString() ) );
+	}
+
 	Add( pRule );
 
-	if ( bMessage )
+	if ( bMessage && pAddress )
 	{
 		theApp.Message( MSG_NOTICE, IDS_NETWORK_SECURITY_BLOCKED,
-			(LPCTSTR)strAddress );
+			(LPCTSTR)CString( inet_ntoa( *pAddress ) ) );
 	}
 }
 
@@ -313,7 +325,7 @@ bool CSecurity::Complain(const IN_ADDR* pAddress, int nBanLength, int nExpire, i
 //////////////////////////////////////////////////////////////////////
 // CSecurity access check
 
-BOOL CSecurity::IsDenied(IN_ADDR* pAddress, LPCTSTR pszContent)
+BOOL CSecurity::IsDenied(const IN_ADDR* pAddress)
 {
 	CQuickLock oLock( m_pSection );
 
@@ -332,7 +344,46 @@ BOOL CSecurity::IsDenied(IN_ADDR* pAddress, LPCTSTR pszContent)
 				m_pRegExpRules.RemoveAt( posRegExp );
 			delete pRule;
 		}
-		else if ( pRule->Match( pAddress, pszContent ) )
+		else if ( pRule->Match( pAddress ) )
+		{
+			pRule->m_nToday ++;
+			pRule->m_nEver ++;
+
+			if ( pRule->m_nExpire > CSecureRule::srSession &&
+				pRule->m_nExpire < nNow + 300 )
+				// Add 5 min penalty for early access
+				pRule->m_nExpire = nNow + 300;
+
+			if ( pRule->m_nAction == CSecureRule::srAccept )
+				return FALSE;
+			else if ( pRule->m_nAction == CSecureRule::srDeny )
+				return TRUE;
+		}
+	}
+
+	return m_bDenyPolicy;
+}
+
+BOOL CSecurity::IsDenied(LPCTSTR pszContent)
+{
+	CQuickLock oLock( m_pSection );
+
+	DWORD nNow = static_cast< DWORD >( time( NULL ) );
+
+	for ( POSITION pos = GetIterator() ; pos ; )
+	{
+		POSITION posLast = pos;
+		CSecureRule* pRule = GetNext( pos );
+
+		if ( pRule->IsExpired( nNow ) )
+		{
+			m_pRules.RemoveAt( posLast );
+			POSITION posRegExp = m_pRegExpRules.Find( pRule );
+			if ( posRegExp )
+				m_pRegExpRules.RemoveAt( posRegExp );
+			delete pRule;
+		}
+		else if ( pRule->Match( pszContent ) )
 		{
 			pRule->m_nToday ++;
 			pRule->m_nEver ++;
@@ -354,21 +405,43 @@ BOOL CSecurity::IsDenied(IN_ADDR* pAddress, LPCTSTR pszContent)
 
 //////////////////////////////////////////////////////////////////////
 // CSecurity check file size, hash
-BOOL CSecurity::IsDenied(CString sName, QWORD nSize, const Hashes::Sha1Hash& oSHA1, 
-						 const Hashes::Ed2kHash& oED2K)
+
+BOOL CSecurity::IsDenied(const CShareazaFile* pFile)
 {
-	if ( LPCTSTR pszExt = PathFindExtension( (LPCTSTR)sName ) )
+	CQuickLock oLock( m_pSection );
+
+	DWORD nNow = static_cast< DWORD >( time( NULL ) );
+
+	for ( POSITION pos = GetIterator() ; pos ; )
 	{
-		CString strExtension;
-		pszExt++;
-		strExtension.Format( _T("size:%s:%I64i"), pszExt, nSize );
-		if ( IsDenied( NULL, strExtension ) )
-			return TRUE;
+		POSITION posLast = pos;
+		CSecureRule* pRule = GetNext( pos );
+
+		if ( pRule->IsExpired( nNow ) )
+		{
+			m_pRules.RemoveAt( posLast );
+			POSITION posRegExp = m_pRegExpRules.Find( pRule );
+			if ( posRegExp )
+				m_pRegExpRules.RemoveAt( posRegExp );
+			delete pRule;
+		}
+		else if ( pRule->Match( pFile ) )
+		{
+			pRule->m_nToday ++;
+			pRule->m_nEver ++;
+
+			if ( pRule->m_nExpire > CSecureRule::srSession &&
+				pRule->m_nExpire < nNow + 300 )
+				// Add 5 min penalty for early access
+				pRule->m_nExpire = nNow + 300;
+
+			if ( pRule->m_nAction == CSecureRule::srAccept )
+				return FALSE;
+			else if ( pRule->m_nAction == CSecureRule::srDeny )
+				return TRUE;
+		}
 	}
-	if ( oSHA1.isValid() && IsDenied( NULL, oSHA1.toUrn() ) )
-		return TRUE;
-	if ( oED2K.isValid() && IsDenied( NULL, oED2K.toUrn() ) )
-		return TRUE;
+
 	return m_bDenyPolicy;
 }
 
@@ -736,7 +809,7 @@ void CSecureRule::Reset()
 //////////////////////////////////////////////////////////////////////
 // CSecureRule expiry check
 
-BOOL CSecureRule::IsExpired(DWORD nNow, BOOL bSession)
+BOOL CSecureRule::IsExpired(DWORD nNow, BOOL bSession) const
 {
 	if ( m_nExpire == srIndefinite ) return FALSE;
 	if ( m_nExpire == srSession ) return bSession;
@@ -746,11 +819,8 @@ BOOL CSecureRule::IsExpired(DWORD nNow, BOOL bSession)
 //////////////////////////////////////////////////////////////////////
 // CSecureRule match
 
-BOOL CSecureRule::Match(const IN_ADDR* pAddress, LPCTSTR pszContent)
+BOOL CSecureRule::Match(const IN_ADDR* pAddress) const
 {
-	if ( IsExpired( (DWORD)time( NULL ) ) )
-		return FALSE;
-
 	if ( m_nType == srAddress && pAddress != NULL )
 	{
 		DWORD* pBase = (DWORD*)m_nIP;
@@ -760,12 +830,20 @@ BOOL CSecureRule::Match(const IN_ADDR* pAddress, LPCTSTR pszContent)
 // This only works if IP's are &ed before entered in the list
 		if ( ( ( *pTest ) & ( *pMask ) ) == ( *pBase ) )
 		{
-			return TRUE;
+			return ! IsExpired( (DWORD)time( NULL ) );
 		}
 	}
-	else if ( m_nType == srContent && pszContent != NULL && m_pContent != NULL )
+	return FALSE;
+}
+
+BOOL CSecureRule::Match(LPCTSTR pszContent) const
+{
+	if ( m_nType == srContent && pszContent != NULL && m_pContent != NULL )
 	{
 		if ( m_nIP[0] == 2 )
+			return FALSE;
+
+		if ( IsExpired( (DWORD)time( NULL ) ) )
 			return FALSE;
 
 		for ( LPCTSTR pszFilter = m_pContent ; *pszFilter ; )
@@ -784,14 +862,39 @@ BOOL CSecureRule::Match(const IN_ADDR* pAddress, LPCTSTR pszContent)
 			pszFilter += _tcslen( pszFilter ) + 1;
 		}
 
-		if ( m_nIP[0] == 1 ) return TRUE;
+		if ( m_nIP[0] == 1 )
+			return TRUE;
+	}
+
+	return FALSE;
+}
+
+BOOL CSecureRule::Match(const CShareazaFile* pFile) const
+{
+	if ( m_nType == srContent && pFile != NULL && m_pContent != NULL )
+	{
+		if ( LPCTSTR pszExt = PathFindExtension( (LPCTSTR)pFile->m_sName ) )
+		{
+			CString strExtension;
+			pszExt++;
+			strExtension.Format( _T("size:%s:%I64i"), pszExt, pFile->m_nSize );
+			if ( Match( strExtension ) )
+				return TRUE;
+		}
+
+		return
+			( pFile->m_oSHA1  && Match( pFile->m_oSHA1.toUrn() ) ) ||
+			( pFile->m_oED2K  && Match( pFile->m_oED2K.toUrn() ) ) ||
+			( pFile->m_oTiger && Match( pFile->m_oTiger.toUrn() ) ) ||
+			( pFile->m_oMD5   && Match( pFile->m_oMD5.toUrn() ) ) ||
+			( pFile->m_oBTH   && Match( pFile->m_oBTH.toUrn() ) );
 	}
 
 	return FALSE;
 }
 
 BOOL CSecureRule::Match(CQuerySearch::const_iterator itStart, 
-						CQuerySearch::const_iterator itEnd, LPCTSTR pszContent)
+						CQuerySearch::const_iterator itEnd, LPCTSTR pszContent) const
 {
 	CString strFilter = GetRegExpFilter( itStart, itEnd );
 	if ( strFilter.GetLength() )
@@ -902,7 +1005,8 @@ CString CSecureRule::GetContentWords()
 
 // Build a regular expression filter from the search query words
 // Returns an empty string if not applied or if the filter was invalid
-CString CSecureRule::GetRegExpFilter(CQuerySearch::const_iterator itStart, CQuerySearch::const_iterator itEnd)
+CString CSecureRule::GetRegExpFilter(CQuerySearch::const_iterator itStart,
+									 CQuerySearch::const_iterator itEnd) const
 {
 	if ( m_nIP[ 0 ] != 2 ) return CString();
 

shareaza/Security.h
@@ -69,12 +69,22 @@ public:
 	void			Remove(CSecureRule* pRule);
 	void			MoveUp(CSecureRule* pRule);
 	void			MoveDown(CSecureRule* pRule);
-	void			Ban(const IN_ADDR* pAddress, int nBanLength, BOOL bMessage = TRUE);
+
+	inline void		Ban(const IN_ADDR* pAddress, int nBanLength, BOOL bMessage = TRUE)
+	{
+		BanHelper( pAddress, NULL, nBanLength, bMessage );
+	}
+
+	inline void		Ban(const CShareazaFile* pFile, int nBanLength, BOOL bMessage = TRUE)
+	{
+		BanHelper( NULL, pFile, nBanLength, bMessage );
+	}
+
 	bool			Complain(const IN_ADDR* pAddress, int nBanLength = ban5Mins, int nExpire = 30, int nCount = 3);
 	void			Clear();
-	BOOL			IsDenied(IN_ADDR* pAddress, LPCTSTR pszContent = NULL);
-	BOOL			IsDenied(CString sName, QWORD nSize, const Hashes::Sha1Hash& oSHA1, 
-							 const Hashes::Ed2kHash& oED2K);
+	BOOL			IsDenied(const IN_ADDR* pAddress);
+	BOOL			IsDenied(LPCTSTR pszContent);
+	BOOL			IsDenied(const CShareazaFile* pFile);
 	BOOL			IsDenied(CQuerySearch::const_iterator itStart, 
 							 CQuerySearch::const_iterator itEnd, LPCTSTR pszContent);
 	void			Expire();
@@ -83,6 +93,7 @@ public:
 	BOOL			Import(LPCTSTR pszFile);
 
 protected:
+	void			BanHelper(const IN_ADDR* pAddress, const CShareazaFile* pFile, int nBanLength, BOOL bMessage);
 	CSecureRule*	GetGUID(const GUID& oGUID) const;
 	CXMLElement*	ToXML(BOOL bRules = TRUE);
 	BOOL			FromXML(CXMLElement* pXML);
@@ -121,13 +132,15 @@ public:
 	void	Remove();
 	void	Reset();
 	void	MaskFix();
-	BOOL	IsExpired(DWORD nNow, BOOL bSession = FALSE);
-	BOOL	Match(const IN_ADDR* pAddress, LPCTSTR pszContent = NULL);
+	BOOL	IsExpired(DWORD nNow, BOOL bSession = FALSE) const;
+	BOOL	Match(const IN_ADDR* pAddress) const;
+	BOOL	Match(LPCTSTR pszContent) const;
+	BOOL	Match(const CShareazaFile* pFile) const;
 	BOOL	Match(CQuerySearch::const_iterator itStart, 
-				  CQuerySearch::const_iterator itEnd, LPCTSTR pszContent);
+				  CQuerySearch::const_iterator itEnd, LPCTSTR pszContent) const;
 	void	SetContentWords(const CString& strContent);
 	CString	GetRegExpFilter(CQuerySearch::const_iterator itStart, 
-							CQuerySearch::const_iterator itEnd);
+							CQuerySearch::const_iterator itEnd) const;
 	CString	GetContentWords();
 
 public:

shareaza/ShakeNeighbour.cpp
@@ -1842,5 +1842,5 @@ BOOL CShakeNeighbour::IsClientBanned()
 	if ( _tcsistr( m_sUserAgent, _T("i2hub 2.0") ) )					return TRUE;
 
 	// Check by content filter
-	return Security.IsDenied( NULL, m_sUserAgent );
+	return Security.IsDenied( m_sUserAgent );
 }

commit 0ef316999f2101325cc21f4cecbd1fc836e4d90f

Fixed search hit counters broken in r7112 (Shareaza 2.4.0.0).

shareaza/MatchObjects.cpp
@@ -1,7 +1,7 @@
 //
 // MatchObjects.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2008.
+// Copyright (c) Shareaza Development Team, 2002-2009.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -105,6 +105,7 @@ CMatchList::CMatchList(CBaseMatchWnd* pParent) : m_pParent( pParent )
 
 	m_pFiles			= NULL;
 	m_nFiles			= 0;
+
 	m_nItems			= 0;
 	m_nFilteredFiles	= 0;
 	m_nFilteredHits		= 0;
@@ -153,6 +154,43 @@ CMatchList::~CMatchList()
 	delete [] m_pFiles;
 
 	delete m_pResultFilters;
+}
+
+//////////////////////////////////////////////////////////////////////
+// CMatchList update stats
+
+void CMatchList::UpdateStats()
+{
+	m_nItems = 0;
+	m_nFilteredFiles = 0;
+	m_nFilteredHits = 0;
+	m_nGnutellaHits = 0;
+	m_nED2KHits = 0;
+
+	for ( DWORD i = 0 ; i < m_nFiles ; ++i )
+	{
+		if ( DWORD nItemCount = m_pFiles[ i ]->GetItemCount() )
+		{
+			m_nItems += nItemCount;
+			m_nFilteredFiles ++;
+			m_nFilteredHits += m_pFiles[ i ]->m_nFiltered;
+			for ( CQueryHit* pHit = m_pFiles[ i ]->GetHits(); pHit; pHit = pHit->m_pNext )
+			{
+				switch ( pHit->m_nProtocol )
+				{
+				case PROTOCOL_G1:
+				case PROTOCOL_G2:
+					m_nGnutellaHits ++;
+					break;
+				case PROTOCOL_ED2K:
+					m_nED2KHits ++;
+					break;
+				default:
+					;
+				}
+			}
+		}
+	}
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -269,8 +307,7 @@ void CMatchList::AddHits(const CQueryHit* pHits, CQuerySearch* pFilter)
 			pFile = FindFileAndAddHit( pHit, fSize, &Stats );
 		}
 
-		bool bExistingFile = ( pFile != NULL );
-		if ( bExistingFile )
+		if ( pFile )
 		{
 			// New hit for the existing file
 			pMap = m_pFiles;
@@ -326,26 +363,6 @@ void CMatchList::AddHits(const CQueryHit* pHits, CQuerySearch* pFilter)
 				UpdateRange( m_nFiles );
 				m_pFiles[ m_nFiles++ ] = pFile;
 			}
-
-			if ( DWORD nItemCount = pFile->GetItemCount() )
-			{
-				m_nItems += nItemCount;
-				m_nFilteredFiles ++;
-				m_nFilteredHits += pFile->m_nFiltered;
-				switch ( pHit->m_nProtocol )
-				{
-				case PROTOCOL_G1:
-				case PROTOCOL_G2:
-					m_nGnutellaHits += pFile->m_nFiltered;
-					break;
-				case PROTOCOL_ED2K:
-					m_nED2KHits += pFile->m_nFiltered;
-					break;
-				default:
-					ASSERT(FALSE);
-					break;
-				}
-			}
 		}
 
 		if ( ! Stats.bHadSHA1 && pFile->m_oSHA1 )
@@ -379,6 +396,8 @@ void CMatchList::AddHits(const CQueryHit* pHits, CQuerySearch* pFilter)
 			*pMap = pFile;
 		}
 	}
+
+	UpdateStats();
 }
 
 CMatchFile* CMatchList::FindFileAndAddHit(CQueryHit* pHit, const findType nFindFlag, FILESTATS* Stats)
@@ -441,9 +460,6 @@ CMatchFile* CMatchList::FindFileAndAddHit(CQueryHit* pHit, const findType nFindF
 			bool bHadBTH = pSeek->m_oBTH;
 			bool bHadMD5 = pSeek->m_oMD5;
 
-			if (pSeek->m_bExpanded)
-				m_nItems -= pSeek->GetItemCount();
-
 			if ( pSeek->Add( pHit, ( nFindFlag != fSize ) ) )
 			{
 				Stats->bHadSHA1	 |= bHadSHA1;
@@ -451,16 +467,11 @@ CMatchFile* CMatchList::FindFileAndAddHit(CQueryHit* pHit, const findType nFindF
 				Stats->bHadED2K	 |= bHadED2K;
 				Stats->bHadBTH	 |= bHadBTH;
 				Stats->bHadMD5	 |= bHadMD5;
-				if (pSeek->m_bExpanded)
-					m_nItems += pSeek->GetItemCount();
 
 				return pSeek;
 			}
 			//else
 				// TODO: Equal hashes for different files or bad hit
-
-			if (pSeek->m_bExpanded)
-				m_nItems += pSeek->GetItemCount();
 		}
 
 		if ( nFindFlag == fSize )
@@ -569,12 +580,8 @@ void CMatchList::Clear()
 	}
 
 	m_nFiles			= 0;
-	m_nItems			= 0;
-	m_nFilteredFiles	= 0;
-	m_nFilteredHits		= 0;
-	m_nGnutellaHits		= 0;
-	m_nED2KHits			= 0;
 
+	UpdateStats();
 	UpdateRange();
 }
 
@@ -861,25 +868,16 @@ void CMatchList::Filter()
 		}
 	}
 
-	CMatchFile** pLoop = m_pFiles;
-
-	m_nItems			= 0;
-	m_nFilteredFiles	= 0;
-	m_nFilteredHits		= 0;
-
-	for ( DWORD nCount = m_nFiles, nItems = 0 ; nCount ; nCount--, pLoop++ )
+	for ( DWORD i = 0 ; i < m_nFiles ; ++i )
 	{
-		if ( ( nItems = (*pLoop)->Filter() ) != 0 )
+		if ( ! m_pFiles[ i ]->Filter() )
 		{
-			m_nItems += nItems;
-			m_nFilteredFiles ++;
-			m_nFilteredHits += (*pLoop)->m_nFiltered;
-		}
-		else
-		{
-			if ( (*pLoop)->m_bSelected ) Select( *pLoop, NULL, FALSE );
+			if ( m_pFiles[ i ]->m_bSelected )
+				Select( m_pFiles[ i ], NULL, FALSE );
 		}
 	}
+
+	UpdateStats();
 
 	SetSortColumn( m_nSortColumn, m_bSortDir < 0 );
 	UpdateRange();
@@ -1249,27 +1247,6 @@ void CMatchList::Serialize(CArchive& ar)
 			CMatchFile* pFile = new CMatchFile( this );
 			m_pFiles[ nFile ] = pFile;
 			pFile->Serialize( ar, nVersion );
-
-			for ( CQueryHit* pHit = pFile->GetHits(); pHit; pHit = pHit->m_pNext )
-			{
-				switch ( pHit->m_nProtocol )
-				{
-				case PROTOCOL_G1:
-				case PROTOCOL_G2:
-					m_nGnutellaHits++;
-					break;
-				case PROTOCOL_ED2K:
-					m_nED2KHits++;
-					break;
-				case PROTOCOL_BT:
-				case PROTOCOL_FTP:
-				case PROTOCOL_HTTP:
-				case PROTOCOL_NULL:
-				case PROTOCOL_ANY:
-				default:
-					;
-				}
-			}
 
 			CMatchFile** pMap = m_pSizeMap + (DWORD)( pFile->m_nSize & 0xFF );
 			pFile->m_pNextSize = *pMap;
@@ -1752,19 +1729,7 @@ DWORD CMatchFile::Filter()
 		}
 	}
 
-	if ( m_pBest == NULL ) return 0;	// If we filtered all hits, don't try to display
-	if ( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE ) return 0;
-	if ( m_pList->m_bFilterDRM && m_bDRM ) return 0;
-	if ( m_pList->m_bFilterSuspicious && m_bSuspicious ) return 0;
-
-	if ( m_nSources < m_pList->m_nFilterSources )
-		return 0;
-	// if ( m_nFiltered < m_pList->m_nFilterSources ) return 0;
-
-	if ( m_nFiltered == 1 || ! m_bExpanded )
-		return 1;
-	else
-		return m_nFiltered + 1;
+	return GetItemCount();
 }
 
 //////////////////////////////////////////////////////////////////////

shareaza/MatchObjects.h
@@ -1,7 +1,7 @@
 //
 // MatchObjects.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2008.
+// Copyright (c) Shareaza Development Team, 2002-2009.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -74,13 +74,13 @@ public:
 	CSchema*		m_pSchema;
 	BOOL			m_bNew;
 	CResultFilters*	m_pResultFilters;
-	CMatchFile**	m_pFiles;
-	DWORD			m_nFiles;
-	DWORD			m_nItems;
-	DWORD			m_nFilteredFiles;
-	DWORD			m_nFilteredHits;
-	DWORD			m_nGnutellaHits;
-	DWORD			m_nED2KHits;
+	CMatchFile**	m_pFiles;				// File list
+	DWORD			m_nFiles;				// File list size
+	DWORD			m_nItems;				// Total number of visible items (filtered files + expanded hits)
+	DWORD			m_nFilteredFiles;		// Total number of filtered files
+	DWORD			m_nFilteredHits;		// Total number of filtered files hits
+	DWORD			m_nGnutellaHits;		// Total number of filtered files hits got by G1/G2
+	DWORD			m_nED2KHits;			// Total number of filtered files hits got by ED2K
 	BOOL			m_bUpdated;
 	DWORD			m_nUpdateMin;
 	DWORD			m_nUpdateMax;
@@ -111,6 +111,7 @@ protected:
 	};
 	
 public:
+	void		UpdateStats();
 	void		AddHits(const CQueryHit* pHits, CQuerySearch* pFilter = NULL);
 	DWORD		FileToItem(CMatchFile* pFile);
 	void		Clear();
@@ -207,18 +208,15 @@ public:
 	
 	inline DWORD GetItemCount()
 	{
-		if ( ( m_pList->m_bFilterDRM && m_bDRM ) ||
-			( m_pList->m_bFilterSuspicious && m_bSuspicious ) ||
-			( m_nSources < m_pList->m_nFilterSources ) ||
-			( m_pBest == NULL ) ||
-			( m_pList->m_bFilterLocal && GetLibraryStatus() == TRI_FALSE ) ) return 0;
-
-		if ( m_nFiltered == 1 || ! m_bExpanded )
+		DWORD nFiltered = GetFilteredCount();
+		if ( nFiltered == 0 )
+			return 0;
+		else if ( nFiltered == 1 || ! m_bExpanded )
 			return 1;
 		else
-			return m_nFiltered + 1;
+			return nFiltered + 1;
 	}
-	
+
 //	int			GetRating() const;
 	DWORD		Filter();
 	void		Added(CQueryHit* pHit);