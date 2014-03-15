sysfce2/Shareaza

commit 3b94e6d68597ba5508deb8e5ccb357cfbe604da4

It's now possible to emulate Gnutella 1 or 2 packet processing by pressing "Ctrl+H" on neighbor (in Network tab) and specifying packet as hexadecimal string i.e. "00 01 02 03 ...". Added new CHexDialog class.
git-svn-id: svn://svn.code.sf.net/p/shareaza/code/trunk@9361 3231ed4b-2112-0410-84e0-8c3944a40730

shareaza/G1Neighbour.cpp
@@ -1,7 +1,7 @@
 //
 // G1Neighbour.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2012.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -256,7 +256,17 @@ BOOL CG1Neighbour::ProcessPackets()
 
 	// Point pInput at the buffer that has readable data from the remote computer
 	CBuffer* pInput = m_pZInput ? m_pZInput : pInputLocked;
+	
+	if ( ProcessPackets( pInput ) )
+		return TRUE;
 
+	// Something in the loop set bSuccess to false
+	Close( 0 );   // Close the connection to this remote computer
+	return FALSE; // Report error
+}
+
+BOOL CG1Neighbour::ProcessPackets(CBuffer* pInput)
+{
 	// Start out with bSuccess true and loop until it gets set to false
 	BOOL bSuccess = TRUE;
 	for ( ; bSuccess ; ) // This is the same thing as while ( bSuccess )
@@ -290,12 +300,7 @@ BOOL CG1Neighbour::ProcessPackets()
 		pInput->Remove( nLength );
 	}
 
-	// If the loop read all the packets from the input buffer without an error, report success
-	if ( bSuccess ) return TRUE;
-
-	// Something in the loop set bSuccess to false
-	Close( 0 );   // Close the connection to this remote computer
-	return FALSE; // Report error
+	return bSuccess;
 }
 
 //////////////////////////////////////////////////////////////////////

shareaza/G1Neighbour.h
@@ -1,7 +1,7 @@
 //
 // G1Neighbour.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2012.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -80,6 +80,8 @@ public:
 
 	// Push packet
 	void SendG2Push(const Hashes::Guid& oGUID, CPacket* pPacket);
+
+	virtual BOOL	ProcessPackets(CBuffer* pInput);
 
 protected:
 

shareaza/G2Neighbour.cpp
@@ -1,7 +1,7 @@
 //
 // G2Neighbour.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2012.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -285,6 +285,15 @@ BOOL CG2Neighbour::ProcessPackets()
 
 	CBuffer* pInput = m_pZInput ? m_pZInput : pInputLocked;
 
+	if ( ProcessPackets( pInput ) )
+		return TRUE;
+
+	Close( 0 );
+	return FALSE;
+}
+
+BOOL CG2Neighbour::ProcessPackets(CBuffer* pInput)
+{
 	BOOL bSuccess = TRUE;
 	while ( bSuccess && pInput->m_nLength )
 	{
@@ -350,11 +359,7 @@ BOOL CG2Neighbour::ProcessPackets()
 		pInput->Remove( nLength + nLenLen + nTypeLen + 2 );
 	}
 
-	if ( bSuccess )
-		return TRUE;
-
-	Close( 0 );
-	return FALSE;
+	return bSuccess;
 }
 
 //////////////////////////////////////////////////////////////////////

shareaza/G2Neighbour.h
@@ -1,7 +1,7 @@
 //
 // G2Neighbour.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2011.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -40,6 +40,7 @@ public:
 	virtual BOOL	SendQuery(const CQuerySearch* pSearch, CPacket* pPacket, BOOL bLocal);
 	virtual DWORD	GetUserCount() const { return m_nLeafCount; }
 	virtual DWORD	GetUserLimit() const { return m_nLeafLimit; }
+	virtual BOOL	ProcessPackets(CBuffer* pInput);
 
 	BOOL			OnPing(CG2Packet* pPacket, BOOL bTCP = TRUE);
 	BOOL			OnPong(CG2Packet* pPacket, BOOL bTCP = TRUE);

shareaza/Neighbour.h
@@ -1,7 +1,7 @@
 //
 // Neighbour.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2011.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -141,6 +141,8 @@ public:
 	virtual DWORD	GetUserCount() const { return 0; }
 	// Returns hub/server leaf/user limit
 	virtual DWORD	GetUserLimit() const { return 0; }
+	// Process packets from input buffer
+	virtual BOOL	ProcessPackets(CBuffer* /*pInput*/) { return TRUE; }
 
 protected:
 	virtual BOOL OnRun();

shareaza/DlgHex.cpp
@@ -1,0 +1,125 @@
+//
+// DlgHex.cpp
+//
+// Copyright (c) Shareaza Development Team, 2014.
+// This file is part of SHAREAZA (shareaza.sourceforge.net)
+//
+// Shareaza is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// Shareaza is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Shareaza; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#include "stdafx.h"
+#include "Shareaza.h"
+#include "DlgHex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+IMPLEMENT_DYNAMIC(CHexDlg, CSkinDialog)
+
+BEGIN_MESSAGE_MAP(CHexDlg, CSkinDialog)
+END_MESSAGE_MAP()
+
+CHexDlg::CHexDlg(CWnd* pParent /*=NULL*/)
+	: CSkinDialog	( CHexDlg::IDD, pParent )
+{
+}
+
+CHexDlg::~CHexDlg()
+{
+}
+
+void CHexDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CSkinDialog::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_HEX, m_sHex);
+}
+
+BOOL CHexDlg::OnInitDialog()
+{
+	CSkinDialog::OnInitDialog();
+
+	SkinMe( _T("CHexDlg"), IDR_MAINFRAME );
+
+	return TRUE;
+}
+
+void CHexDlg::OnOK()
+{
+	UpdateData();
+
+	m_pBuffer.Clear();
+
+	m_sHex.Remove( _T(' ') );
+	m_sHex.Remove( _T('\t') );
+	m_sHex.Remove( _T('\r') );
+	m_sHex.Remove( _T('\n') );
+
+	if ( size_t length = m_sHex.GetLength() )
+	{
+		if ( ( length & 1 ) != 0 )
+		{
+			AfxMessageBox( _T("String has wrong even length!") );
+			return;
+		}
+
+		m_pBuffer.EnsureBuffer( length / 2 );
+
+		for ( size_t i = 0; i < length; ++i )
+		{
+			BYTE b = 0;
+			TCHAR c = m_sHex.GetAt( i );
+			if ( c >= '0' && c <= '9' )
+				b = BYTE( ( c - '0' ) << 4 );
+			else if ( c >= 'A' && c <= 'F' )
+				b = BYTE( ( c - 'A' + 10 ) << 4 );
+			else if ( c >= 'a' && c <= 'f' )
+				b = BYTE( ( c - 'a' + 10 ) << 4 );
+			else
+			{
+				CString sMsg;
+				sMsg.Format( _T("Found wrong symbol at position: %d"), i );
+				AfxMessageBox( sMsg );
+				return;
+			}
+			++i;
+			c = m_sHex.GetAt( i );
+			if ( c >= '0' && c <= '9' )
+				b |= BYTE( c - '0' );
+			else if ( c >= 'A' && c <= 'F' )
+				b |= BYTE( c - 'A' + 10 );
+			else if ( c >= 'a' && c <= 'f' )
+				b |= BYTE( c - 'a' + 10 );
+			else
+			{
+				CString sMsg;
+				sMsg.Format( _T("Found wrong symbol at position: %d"), i );
+				AfxMessageBox( sMsg );
+				return;
+			}
+
+			m_pBuffer.Add( &b, 1 );
+		}
+	}
+	else
+	{
+		AfxMessageBox( _T("Empty string!") );
+		return;
+	}
+
+	CSkinDialog::OnOK();
+}

shareaza/DlgHex.h
@@ -1,0 +1,49 @@
+//
+// DlgHex.h
+//
+// Copyright (c) Shareaza Development Team, 2014.
+// This file is part of SHAREAZA (shareaza.sourceforge.net)
+//
+// Shareaza is free software; you can redistribute it
+// and/or modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2 of
+// the License, or (at your option) any later version.
+//
+// Shareaza is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Shareaza; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#pragma once
+
+#include "DlgSkinDialog.h"
+#include "Buffer.h"
+
+
+class CHexDlg : public CSkinDialog
+{
+	DECLARE_DYNAMIC(CHexDlg)
+
+public:
+	CHexDlg(CWnd* pParent = NULL);   // standard constructor
+	virtual ~CHexDlg();
+
+	enum { IDD = IDD_DEBUG_HEX };
+
+	CBuffer* GetData() { return &m_pBuffer; }
+
+protected:
+	CString m_sHex;
+	CBuffer m_pBuffer;
+
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	virtual BOOL OnInitDialog();
+	virtual void OnOK();
+
+	DECLARE_MESSAGE_MAP()
+};

shareaza/Shareaza.rc
@@ -1927,6 +1927,16 @@ BEGIN
                     "msctls_progress32",WS_BORDER,8,78,234,10
 END
 
+IDD_DEBUG_HEX DIALOGEX 0, 0, 316, 184
+STYLE DS_SETFONT | DS_MODALFRAME | DS_FIXEDSYS | WS_POPUP | WS_CAPTION | WS_SYSMENU
+CAPTION "Enter hexadecimal string"
+FONT 8, "MS Shell Dlg", 400, 0, 0x1
+BEGIN
+    DEFPUSHBUTTON   "OK",IDOK,183,161,60,16
+    PUSHBUTTON      "Cancel",IDCANCEL,249,161,60,16
+    EDITTEXT        IDC_EDIT_HEX,7,7,302,146,ES_MULTILINE | ES_AUTOVSCROLL | ES_WANTRETURN | WS_VSCROLL
+END
+
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -2701,6 +2711,14 @@ BEGIN
         HORZGUIDE, 72
         HORZGUIDE, 78
         HORZGUIDE, 88
+    END
+
+    IDD_DEBUG_HEX, DIALOG
+    BEGIN
+        LEFTMARGIN, 7
+        RIGHTMARGIN, 309
+        TOPMARGIN, 7
+        BOTTOMMARGIN, 177
     END
 END
 #endif    // APSTUDIO_INVOKED

commit 0325105207fa31846d4ac6fe65b28693a0f505fc

Re-factored neighbors ProcessPackets() methods (fix for strange virtual method bug introduced in r9361).

shareaza/DCNeighbour.cpp
@@ -1,7 +1,7 @@
 //
 // DCNeighbour.cpp
 //
-// Copyright (c) Shareaza Development Team, 2010-2011.
+// Copyright (c) Shareaza Development Team, 2010-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -145,52 +145,65 @@ BOOL CDCNeighbour::ConnectTo(const IN_ADDR* pAddress, WORD nPort, BOOL bAutomati
 
 BOOL CDCNeighbour::OnRead()
 {
-	if ( ! CNeighbour::OnRead() )
+	CNeighbour::OnRead();
+
+	return ProcessPackets();
+}
+
+BOOL CDCNeighbour::ProcessPackets()
+{
+	CLockedBuffer pInputLocked( GetInput() );
+
+	if ( m_pZInput )
+	{
+		if ( m_pZInput->m_nLength == 0 )
+		{
+			if ( m_bZInputEOS )
+			{
+				// Got "End of Stream" so turn decompression off
+				m_bZInputEOS = FALSE;
+				if ( m_pZSInput )
+					m_pZInput->InflateStreamCleanup( m_pZSInput );
+				delete m_pZInput;
+				m_pZInput = NULL;
+			}
+			return TRUE;
+		}
+	}
+
+	CBuffer* pInput = m_pZInput ? m_pZInput : pInputLocked;
+
+	return ProcessPackets( pInput );
+}
+
+BOOL CDCNeighbour::ProcessPackets(CBuffer* pInput)
+{
+	if ( ! pInput )
 		return FALSE;
 
-	for ( ;; )
+	BOOL bSuccess = TRUE;
+
+	while ( CDCPacket* pPacket = CDCPacket::ReadBuffer( pInput ) )
 	{
-		CDCPacket* pPacket;
+		try
 		{
-			CLockedBuffer pInputLocked( GetInput() );
-
-			if ( m_pZInput )
-			{
-				if ( m_pZInput->m_nLength == 0 )
-				{
-					if ( m_bZInputEOS )
-					{
-						// Got "End of Stream" so turn decompression off
-						m_bZInputEOS = FALSE;
-						if ( m_pZSInput )
-							m_pZInput->InflateStreamCleanup( m_pZSInput );
-						delete m_pZInput;
-						m_pZInput = NULL;
-					}
-					return TRUE;
-				}
-
-				pPacket = CDCPacket::ReadBuffer( m_pZInput );
-			}
-			else
-				pPacket = CDCPacket::ReadBuffer( pInputLocked );
+			bSuccess = OnPacket( pPacket );
+		}
+		catch ( CException* pException )
+		{
+			pException->Delete();
 		}
 
-		if ( ! pPacket )
-			return TRUE;
-
-		pPacket->SmartDump( &m_pHost, FALSE, FALSE, (DWORD_PTR)this );
-
-		m_nInputCount++;
-		m_tLastPacket = GetTickCount();
-
-		BOOL bResult = OnPacket( pPacket );
-
 		pPacket->Release();
+		if ( ! bSuccess )
+			break;
 
-		if ( ! bResult )
-			return FALSE;
+		if ( m_pZInput && m_pZInput != pInput )
+			// Compression just turned on
+			break;
 	}
+
+	return bSuccess;
 }
 
 BOOL CDCNeighbour::Send(CPacket* pPacket, BOOL bRelease, BOOL /*bBuffered*/)
@@ -292,6 +305,11 @@ void CDCNeighbour::OnDropped()
 
 BOOL CDCNeighbour::OnPacket(CDCPacket* pPacket)
 {
+	pPacket->SmartDump( &m_pHost, FALSE, FALSE, (DWORD_PTR)this );
+
+	m_nInputCount++;
+	m_tLastPacket = GetTickCount();
+
 	if ( pPacket->m_nLength < 2  )
 	{
 		return OnPing();

shareaza/DCNeighbour.h
@@ -1,7 +1,7 @@
 //
 // DCNeighbour.h
 //
-// Copyright (c) Shareaza Development Team, 2010-2011.
+// Copyright (c) Shareaza Development Team, 2010-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -38,6 +38,9 @@ public:
 	virtual BOOL	Send(CPacket* pPacket, BOOL bRelease = TRUE, BOOL bBuffered = FALSE);
 	virtual DWORD	GetUserCount() const { return m_oUsers.GetCount(); }
 
+	// Process packets from input buffer
+	virtual BOOL	ProcessPackets(CBuffer* pInput);
+
 	// Send $ConnectToMe command
 	BOOL			ConnectToMe(const CString& sNick);
 
@@ -60,6 +63,9 @@ protected:
 	virtual BOOL	OnConnected();
 	virtual void	OnDropped();
 	virtual BOOL	OnRead();
+
+	// Process packets from internal input buffer
+	virtual BOOL	ProcessPackets();
 
 	// Got DC++ command
 	BOOL			OnPacket(CDCPacket* pPacket);

shareaza/EDNeighbour.cpp
@@ -1,7 +1,7 @@
 //
 // EDNeighbour.cpp
 //
-// Copyright (c) Shareaza Development Team, 2002-2013.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -211,14 +211,28 @@ void CEDNeighbour::OnDropped()
 
 BOOL CEDNeighbour::OnRead()
 {
-	BOOL bSuccess = TRUE;
+	CNeighbour::OnRead();
 
-	if ( ! CNeighbour::OnRead() )
+	return ProcessPackets();
+}
+
+BOOL CEDNeighbour::ProcessPackets()
+{
+	CLockedBuffer pInputLocked( GetInput() );
+
+	CBuffer* pInput = m_pZInput ? m_pZInput : pInputLocked;
+	
+	return ProcessPackets( pInput );
+}
+
+BOOL CEDNeighbour::ProcessPackets(CBuffer* pInput)
+{
+	if ( ! pInput )
 		return FALSE;
 
-	CLockedBuffer pInput( GetInput() );
+	BOOL bSuccess = TRUE;
 
-	while ( CEDPacket* pPacket = CEDPacket::ReadBuffer( m_pZInput ? m_pZInput : pInput ) )
+	while ( CEDPacket* pPacket = CEDPacket::ReadBuffer( pInput ) )
 	{
 		try
 		{
@@ -230,7 +244,8 @@ BOOL CEDNeighbour::OnRead()
 		}
 
 		pPacket->Release();
-		if ( ! bSuccess ) break;
+		if ( ! bSuccess )
+			break;
 	}
 
 	return bSuccess;

shareaza/EDNeighbour.h
@@ -1,7 +1,7 @@
 //
 // EDNeighbour.h
 //
-// Copyright (c) Shareaza Development Team, 2002-2013.
+// Copyright (c) Shareaza Development Team, 2002-2014.
 // This file is part of SHAREAZA (shareaza.sourceforge.net)
 //
 // Shareaza is free software; you can redistribute it
@@ -70,7 +70,14 @@ public:
 	virtual BOOL	Send(CPacket* pPacket, BOOL bRelease = TRUE, BOOL bBuffered = FALSE);
 	virtual DWORD	GetUserCount() const { return m_nUserCount; }
 	virtual DWORD	GetUserLimit() const { return m_nUserLimit; }
+
+	// Process packets from input buffer
+	virtual BOOL	ProcessPackets(CBuffer* pInput);
+
 protected:
+	// Process packets from internal input buffer
+	virtual BOOL	ProcessPackets();
+
 	virtual BOOL	SendQuery(const CQuerySearch* pSearch, CPacket* pPacket, BOOL bLocal);
 	virtual BOOL	OnRun();
 	virtual BOOL	OnConnected();

shareaza/G1Neighbour.cpp
@@ -267,6 +267,9 @@ BOOL CG1Neighbour::ProcessPackets()
 
 BOOL CG1Neighbour::ProcessPackets(CBuffer* pInput)
 {
+	if ( ! pInput )
+		return FALSE;
+
 	// Start out with bSuccess true and loop until it gets set to false
 	BOOL bSuccess = TRUE;
 	for ( ; bSuccess ; ) // This is the same thing as while ( bSuccess )

shareaza/G1Neighbour.h
@@ -81,7 +81,8 @@ public:
 	// Push packet
 	void SendG2Push(const Hashes::Guid& oGUID, CPacket* pPacket);
 
-	virtual BOOL	ProcessPackets(CBuffer* pInput);
+	// Process packets from specified buffer
+	virtual BOOL ProcessPackets(CBuffer* pInput);
 
 protected:
 
@@ -92,8 +93,9 @@ protected:
 
 protected:
 
-	// Read and respond to packets from the remote computer
-	BOOL ProcessPackets();             // Cuts up the recieved data into packets, and calls OnPacket for each one
+	// Process packets from internal input buffer
+	virtual BOOL ProcessPackets();
+
 	BOOL OnPacket(CG1Packet* pPacket); // Sorts the packet and calls one of the methods below
 
 	// Ping and pong packets

shareaza/G2Neighbour.cpp
@@ -116,6 +116,7 @@ CG2Neighbour::~CG2Neighbour()
 BOOL CG2Neighbour::OnRead()
 {
 	CNeighbour::OnRead();
+
 	return ProcessPackets();
 }
 
@@ -294,6 +295,9 @@ BOOL CG2Neighbour::ProcessPackets()
 
 BOOL CG2Neighbour::ProcessPackets(CBuffer* pInput)
 {
+	if ( ! pInput )
+		return FALSE;
+
 	BOOL bSuccess = TRUE;
 	while ( bSuccess && pInput->m_nLength )
 	{

shareaza/G2Neighbour.h
@@ -40,6 +40,8 @@ public:
 	virtual BOOL	SendQuery(const CQuerySearch* pSearch, CPacket* pPacket, BOOL bLocal);
 	virtual DWORD	GetUserCount() const { return m_nLeafCount; }
 	virtual DWORD	GetUserLimit() const { return m_nLeafLimit; }
+
+	// Process packets from specified buffer
 	virtual BOOL	ProcessPackets(CBuffer* pInput);
 
 	BOOL			OnPing(CG2Packet* pPacket, BOOL bTCP = TRUE);
@@ -108,5 +110,7 @@ protected:
 	virtual BOOL	OnRun();
 
 	void			SendStartups();
-	BOOL			ProcessPackets();
+
+	// Process packets from internal input buffer
+	virtual BOOL	ProcessPackets();
 };

shareaza/Neighbour.h
@@ -133,18 +133,27 @@ public:
 	virtual BOOL	ConnectTo(const IN_ADDR* pAddress, WORD nPort, BOOL bAutomatic);
 	virtual BOOL	Send(CPacket* pPacket, BOOL bRelease = TRUE, BOOL bBuffered = FALSE);
 	virtual void	Close(UINT nError = IDS_CONNECTION_CLOSED);
+
 	// Send the buffer then close the socket, record the error given
 	virtual void	DelayClose(UINT nError);
+
 	// Validate query
 	virtual BOOL	SendQuery(const CQuerySearch* pSearch, CPacket* pPacket, BOOL bLocal);
+
 	// Returns hub/server leaf/user count
 	virtual DWORD	GetUserCount() const { return 0; }
+
 	// Returns hub/server leaf/user limit
 	virtual DWORD	GetUserLimit() const { return 0; }
+
 	// Process packets from input buffer
 	virtual BOOL	ProcessPackets(CBuffer* /*pInput*/) { return TRUE; }
 
 protected:
+
+	// Process packets from internal input buffer
+	virtual BOOL	ProcessPackets() { return TRUE; }
+
 	virtual BOOL OnRun();
 	virtual void OnDropped();
 	virtual BOOL OnRead();